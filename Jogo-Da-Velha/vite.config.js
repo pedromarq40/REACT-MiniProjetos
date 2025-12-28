d8b6286e343c941963ff25fa0902e17f054e4ff7
pedromarq40 <pedromarq400@gmail.com>
2025-12-28 20:10:01 -0300

    Correção no deploy

--- Jogo-Da-Velha/vite.config.js
+++ Jogo-Da-Velha/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/REACT---Exerc-cios/Jogo-Da-Velha/dist/',
   plugins: [react()],
 })
